629d2ee4e7c01d668d9ba0840220f9013478345a
Amara bibi <amararashid8888@gmail.com>
2026-01-24 20:50:40 -0800

    Add SuppressLint annotation to homefragment
    Imported android.annotation.SuppressLint and added @SuppressLint("SetTextI18n") to the onCreateView method in homefragment to suppress lint warnings related to setting text directly.

--- app/src/main/java/com/example/hostello/homefragment.java
+++ app/src/main/java/com/example/hostello/homefragment.java
@@ -1,5 +1,6 @@
 package com.example.hostello;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 public class homefragment extends Fragment {
 
+    @SuppressLint("SetTextI18n")
     @Nullable
     @Override
     public View onCreateView(
